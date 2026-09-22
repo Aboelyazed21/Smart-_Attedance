fae70147b092dda787214c4e8b19d0075d1c38fa
ZEZO <nabwalyzydhatm@gmail.com>
2026-09-22 15:56:39 +0300

    Make admin sidebar fixed and responsive

--- src/pages/dashboard/AdminDashboard.jsx
+++ src/pages/dashboard/AdminDashboard.jsx
@@ -1,7 +1,7 @@
 ﻿import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getDashboardReport } from "../../services/api";
-
+import "./AdminDashboard.css";
 function Icon({ type, size = 22 }) {
   const common = {
     width: size,
